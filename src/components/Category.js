040364b40be29a2596e95296dfcbd7f36579b237
hoppare <hoppare@sch.bme.hu>
2021-11-03 20:12:45 +0100

    categories now show how many tasks are in them

--- src/components/Category.js
+++ src/components/Category.js
@@ -10,8 +10,9 @@ function Category(props) {
   return (
       <div >
       <h1 className="text-center mb-3"> 
-          {props.name}
+          {props.name}<span className="text-muted fs-4">({props.tasks.length}) </span>
       </h1>
+      
       <div className=" d-flex flex-column  justify-content-center ">
       {
           props.tasks.map((categoryTask) => (<Item key={categoryTask.id} data={categoryTask} editTask={props.editTask} deleteTask={props.deleteTask}/>))
